740b122baa93d79c92744a92f2aac190949f1757
feihongryan <feihongryan@users.noreply.github.com>
2026-08-04 12:07:10 +0000

    deploy: a8759a725e9525c510257699ef64f12a7548ebb3

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-2bb7c4ac.js',
+                path_to_root + 'searchindex-253ef151.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
